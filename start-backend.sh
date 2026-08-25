59d84e2c703c96b3dec3cb6c9f4e27ab1bcc58a4
Anderson Dourado Cunha <anderson.ictus@gmail.com>
2026-08-25 13:30:56 -0300

    feat: ajustando script de inicialização

--- start-backend.sh
+++ start-backend.sh
@@ -17,14 +17,14 @@ APP_PORT=${APP_PORT:-8000}
 # cria venv se não existir
 if [ ! -d "venv" ]; then
   echo "Criando ambiente virtual..."
-  python -m venv venv
+  uv venv --python 3.11 venv
 fi
 
 echo "### Ativando ambiente virtual..."
-source venv/Scripts/activate
+source venv/bin/activate
 
 echo "### Instalando dependências..."
-pip install -r requirements.txt
+uv pip install -r requirements.txt
 
 echo "### Subindo servidor FastAPI na porta $APP_PORT..."
 uvicorn app.main:app --reload --host 0.0.0.0 --port "$APP_PORT"
